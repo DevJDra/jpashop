e470f61c07006396d5dfd89e61dabccd5fe8231f
DevJDra <jyahn3752@gmail.com>
2022-06-19 20:47:12 +0900

    JPA 홈 화면과 레이아웃

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,5 +20,8 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch){}
+//    public List<Order> findAll(OrderSearch orderSearch){
+//        String jpql = "select o from Order o join o.member m";
+//
+//    }
 }
